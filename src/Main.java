b40c83f374772c7e758ac390a1ec6ee178edb2b0
ARS1802 <arsouza1802@gmail.com>
2026-03-16 08:18:52 -0300

    workInProgress: UI prints result in console

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,9 @@ import ui.GUI;
 
 public class Main {
     public static void main(String[] args) {
-
         TruthTable resposta = Lexer.solveToTruthTable("(p^q)→r");
         Boolean[][] arr = resposta.getTable();
-        imprimirMatriz(arr);
 
         new GUI();
-    }
-
-    public static void imprimirMatriz(Boolean[][] matriz) {
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print(matriz[i][j] + " ");
-            }
-            System.out.println();
-        }
-
     }
 }
